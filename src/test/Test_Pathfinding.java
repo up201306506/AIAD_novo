f20a864eb0f7c2ee57e3bdbf25b26beaba497b60
Tropical <up201306506@gcloud.fe.up.pt>
2016-11-17 17:22:52 +0000

    50%

--- src/test/Test_Pathfinding.java
+++ src/test/Test_Pathfinding.java
@@ -1,11 +1,11 @@
 package test;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import tools.Pathfinding.Map_cell;
-import tools.Pathfinding.Path_finding_map;
+import tools.Pathfinding;
+import tools.Pathfinding.*;
+import application.*;
 
 public class Test_Pathfinding {
 
@@ -30,19 +30,60 @@ public class Test_Pathfinding {
 	
 	@Test
 	public void test_Path_finding_class() {
-
-		Path_finding_map TestMap = new Path_finding_map(2, 7, (byte) 0);
-		
-		System.out.print("x"); 
-		TestMap.display_map_console(1);
-		System.out.print("\ny"); 
-		TestMap.display_map_console(2);
-		System.out.print("\nValue"); 
-		TestMap.display_map_console(0);
 		
 
-		byte[][] test_datamap = {{1,1,1,1},{1,1,1,1},{1,1,1,1}};
+		Path_finding_map TestMap = new Path_finding_map(4, 3, (byte) 0);
+
+		byte[][] test_datamap = {{1,1,1,1},{1,0,0,1},{1,1,1,1}};
+		byte[][] test_datamap_wrong_cols = {{1,1,1},{1,1,1},{1,1,1}};
+		byte[][] test_datamap_wrong_rows = {{1,1,1,1},{1,1,1,1}};
+		
+		
+		//Showing loaded map
+		assertFalse(TestMap.load_map_blocks(test_datamap_wrong_rows));
+		assertFalse(TestMap.display_map_console(0));
+		assertFalse(TestMap.load_map_blocks(test_datamap_wrong_cols));
+		assertFalse(TestMap.display_map_console(0));
+		
+		assertTrue(TestMap.load_map_blocks(test_datamap));
+		
+		
+		assertTrue(TestMap.display_map_console(1));
+		assertTrue(TestMap.display_map_console(2));
+		assertTrue(TestMap.display_map_console(0));
+		
+
 		
 	}
 
+	@Test
+	public void test_Distance_Functions() {
+		byte[][] test_datamap = {{1,1,1,1},{1,0,0,1},{1,0,0,1},{1,1,1,1}};
+		Path_finding_map TestMap = new Path_finding_map(4, 4, test_datamap);
+		
+		
+		assertEquals(Pathfinding.euclidean_distance(TestMap.datamap[1][1], TestMap.datamap[2][2]), Math.sqrt(2), 0);
+		assertEquals(Pathfinding.manhattan_distance(TestMap.datamap[1][1], TestMap.datamap[2][2]), 2, 0);
+		
+		//Checking if implementations are equal
+		/* 
+		for(int i = 0; i < 4; i++)
+			for(int j = 0; j < 4; j++){
+				
+				Taxi testT = new Taxi();
+				Passenger testP = new Passenger();
+				
+				assertEquals(Pathfinding.euclidean_distance(TestMap.datamap[1][1], TestMap.datamap[2][2]), 
+								Pathfinding.euclidean_distance(testT, testP) );
+				assertEquals(Pathfinding.manhattan_distance(TestMap.datamap[1][1], TestMap.datamap[2][2]), 
+								Pathfinding.manhattan_distance(testT, testP));
+			}
+		*/
+	}
+	
+	@Test
+	public void test_Path_Functions() {
+		
+		
+	}
 }
--- src/tools/Pathfinding.java
+++ src/tools/Pathfinding.java
@@ -1,15 +1,18 @@
 package tools;
 
+import application.*;
+
 public class Pathfinding {
 
 	public static class Map_cell{
 		public int x = 0;
 		public int y = 0;
-		public byte block = 0;
+		public byte value = 0;
 		public boolean visited = false;
 		public int effort = Integer.MAX_VALUE;
 		public int distance = Integer.MAX_VALUE;
-		
+
+		//Constructor
 		public Map_cell (int x, int y){
 			this.x = x;
 			this.y = y;
@@ -21,12 +24,14 @@ public class Pathfinding {
 			}
 		}
 	}
-	
+
 	public static class Path_finding_map{
 		public int cols = 0;
 		public int rows = 0;
 		public Map_cell[][] datamap;
-		
+		public boolean loaded = false;
+
+		//Constructor
 		public Path_finding_map( int c, int r, byte fill ){
 			this.cols = c;
 			this.rows = r;
@@ -34,55 +39,106 @@ public class Pathfinding {
 			for(int i = 0; i < rows; i++){
 				for(int j = 0; j < cols; j++){
 					datamap[i][j] = new Map_cell(j, i);
-					datamap[i][j].block = fill;
+					datamap[i][j].value = fill;
 				}
 			}
 		}
-		
-		public boolean valid_map(byte[][] data){
-			int r = data.length;
-			int c = data[0].length;
-			System.out.println(c);
-			System.out.println(r);
-			return false;
+		public Path_finding_map(int c, int r, byte[][] data){
+			this(c, r, (byte) 0);
+			try{
+				if(!load_map_blocks(data))
+					throw new IllegalArgumentException();
+			}
+			catch(IllegalArgumentException e){
+				System.err.println("Could not load map data to Pathfinding class, invalid map");
+			}
+		}
+
+		public boolean valid_map_size(byte[][] data){
+			if(rows != data.length)
+				return false;
 			
-		}
-		public boolean load_map_blocks( int c, int r, byte[][] data ){
-			for(int i = 0; i < rows; i++){
-				for(int j = 0; j < cols; j++){
-					datamap[i][j].block = data[i][j];
-				}
-			}
+			for(int i = 0; i < rows; i++)
+				if(cols != data[i].length)
+					return false;
+						
 			return true;
 		}
-		public void display_map_console(int option){
-			for(int i = 0; i < rows; i++){
-				System.out.print("\n[ ");
-				for(int j = 0; j < cols; j++){
-					switch(option){
-					case 0: System.out.print(datamap[i][j].block + " "); break;
-					case 1: System.out.print(datamap[i][j].x + " "); break;
-					case 2: System.out.print(datamap[i][j].y + " "); break;
-					case 3: System.out.print(datamap[i][j].visited + " "); break;
-					case 4: System.out.print(datamap[i][j].effort + " "); break;
-					case 5: System.out.print(datamap[i][j].distance + " "); break;
-					default: System.out.print(datamap[i][j].block + " "); break;
-					}
-					System.out.print(" ");
-				}
-				System.out.print("]");
-			}
-		}
 		
+		public boolean load_map_blocks(byte[][] data ){
+			if(!valid_map_size(data))
+				return false;
+			for(int i = 0; i < rows; i++){
+				for(int j = 0; j < cols; j++){
+					datamap[i][j].value = data[i][j];
+				}
+			}
+			loaded = true;
+			return true;
+		}
+		public boolean display_map_console(int option){
+			if(loaded)
+			{
+				switch(option){
+					case 1: System.out.print("x");  break;
+					case 2: System.out.print("y");  break;
+					case 3: System.out.print("visited");  break;
+					case 4: System.out.print("effort");  break;
+					case 5: System.out.print("distance");  break;
+					default: System.out.print("block");  break;
+					
+				}
+
+				for(int i = 0; i < rows; i++){
+					System.out.print("\n[ ");
+					for(int j = 0; j < cols; j++){
+						switch(option){
+						case 1: System.out.print(datamap[i][j].x + " "); break;
+						case 2: System.out.print(datamap[i][j].y + " "); break;
+						case 3: System.out.print(datamap[i][j].visited + " "); break;
+						case 4: System.out.print(datamap[i][j].effort + " "); break;
+						case 5: System.out.print(datamap[i][j].distance + " "); break;
+						default: System.out.print(datamap[i][j].value + " "); break;
+						}
+						System.out.print(" ");
+					}
+					System.out.print("]");
+				}
+				System.out.println();
+			}
+			return loaded;
+		}
 	}
+
+
+	/*
+	 * 	Distance functions
+	 */
 	
-	
-	public double euclidean_distance(Map_cell origin, Map_cell destination){
+	//Map Cell
+	public static double euclidean_distance(Map_cell origin, Map_cell destination){
 		return Math.sqrt( (destination.y-origin.y)*(destination.y-origin.y) + (destination.x-origin.x)*(destination.x-origin.x) );	
 	}
-	
-	public int manhattan_distance(Map_cell origin, Map_cell destination){
+	public static int manhattan_distance(Map_cell origin, Map_cell destination){
 		return (Math.abs(destination.x - origin.x) + Math.abs(destination.y - origin.y));	
+	}
+	
+	//Taxi to Passenger
+	public static double euclidean_distance(Taxi origin, Passenger destination){
+		return Math.sqrt( (destination.getYiCoord()-origin.getYCoord())*(destination.getYiCoord()-origin.getYCoord()) + (destination.getXiCoord()-origin.getXCoord())*(destination.getXiCoord()-origin.getXCoord()) );	
+	}
+	public static int manhattan_distance(Taxi origin, Passenger destination){
+		return (Math.abs(destination.getXiCoord() - origin.getXCoord()) + Math.abs(destination.getYiCoord() - origin.getYCoord()));	
+	}
+
+	/*
+	 * 	Path Finding functions
+	 */
+	
+	
+	//change return type to whatever the path type is
+	public static void getPath(Taxi origin, Passenger destination){
+		return;
 	}
 	
 }
